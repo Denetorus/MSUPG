636917a0a6559c1880f241b8aa2b558005232dab
denetorus <gfc-com@mail.ru>
2022-04-20 10:08:02 +0300

    change method loading DB params

--- database/DBMain/DB.php
+++ database/DBMain/DB.php
@@ -6,10 +6,4 @@ use \sketch\database\DBBase;
 
 class DB extends DBBase
 {
-    protected static $DB = null;
-
-    public static function getAttributes()
-    {
-        return include('db_config.php');
-    }
 }
